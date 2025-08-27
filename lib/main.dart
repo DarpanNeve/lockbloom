cc990aed3eb88a32a3c6bab933b681253bdc6e44
darpanneve <darpanneve3@gmail.com>
2025-08-27 19:46:16 +0530

    v1 released to play store

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -6,13 +7,25 @@ import 'package:lockbloom/app/bindings/initial_binding.dart';
 import 'package:lockbloom/app/routes/app_pages.dart';
 import 'package:lockbloom/app/services/theme_service.dart';
 import 'package:lockbloom/app/themes/app_theme.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:upgrader/upgrader.dart';
+import 'package:shorebird_code_push/shorebird_code_push.dart'; // <-- add
 
 import 'app/services/storage_service.dart';
 import 'app/services/encryption_service.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   // Initialize services
   await Get.putAsync(() async => StorageService());
   await Get.putAsync<EncryptionService>(() async {
@@ -21,12 +34,19 @@ void main() async {
     return encryptionService;
   });
   await Get.putAsync(() => ThemeService().init());
+  final shorebirdUpdater = ShorebirdUpdater();
+  final status = await shorebirdUpdater.checkForUpdate();
+
+  if (status == UpdateStatus.outdated) {
+    await shorebirdUpdater.update();
+  }
+
   // Set preferred orientations
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  
+
   runApp(const LockBloomApp());
 }
 
@@ -35,24 +55,28 @@ class LockBloomApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812), // iPhone 13 Pro size
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return GetMaterialApp(
-          title: 'LockBloom',
-          debugShowCheckedModeBanner: false,
-          initialBinding: InitialBinding(),
-          initialRoute: AppPages.INITIAL,
-          getPages: AppPages.routes,
-          theme: AppTheme.lightTheme,
-          darkTheme: AppTheme.darkTheme,
-          themeMode: Get.find<ThemeService>().theme,
-          locale: Get.deviceLocale,
-          fallbackLocale: const Locale('en', 'US'),
-        );
-      },
+    return UpgradeAlert(
+      barrierDismissible: false,
+      showIgnore: false,
+      child: ScreenUtilInit(
+        designSize: const Size(375, 812), // iPhone 13 Pro size
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return GetMaterialApp(
+            title: 'LockBloom',
+            debugShowCheckedModeBanner: false,
+            initialBinding: InitialBinding(),
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: Get.find<ThemeService>().theme,
+            locale: Get.deviceLocale,
+            fallbackLocale: const Locale('en', 'US'),
+          );
+        },
+      ),
     );
   }
 }
